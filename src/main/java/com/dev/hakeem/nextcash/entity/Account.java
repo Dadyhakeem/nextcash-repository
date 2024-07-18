eda520e67b6a2769823899fccdbd6f474518d98b
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-18 02:44:38 -0300

    feat: classe account criado

---
+++ src/main/java/com/dev/hakeem/nextcash/entity/Account.java
@@ -0,0 +1,66 @@
+package com.dev.hakeem.nextcash.entity;
+
+import com.dev.hakeem.nextcash.enums.AccountType;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_account")
+public class Account {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @NotNull(message = "Balance cannot be null")
+    @Column(name = "balance", nullable = false)
+    private Double balance;
+
+    @NotBlank(message = "Financial Institution cannot be blank")
+    @Column(name = "financial_institution", nullable = false)
+    private String financialInstitution;
+
+    @Enumerated(EnumType.STRING)
+    @NotNull(message = "Account Type cannot be null")
+    @Column(name = "account_type", nullable = false)
+    private AccountType accountType;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Account account = (Account) o;
+        return Objects.equals(id, account.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
