0b5b2dfc5cf0742625d46e91f226feb65f68b344
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-18 23:15:59 -0300

    feat: UserCreateResponse.java criado

---
+++ src/main/java/com/dev/hakeem/nextcash/web/response/UserCreateResponse.java
@@ -0,0 +1,23 @@
+package com.dev.hakeem.nextcash.web.response;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter @AllArgsConstructor @NoArgsConstructor
+public class UserCreateResponse {
+
+
+    private Long id;
+
+    @NotBlank(message = "Username cannot be blank")
+    private String username;
+
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Invalid email format")
+    private String email;
+}
