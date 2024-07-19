9ac7eca4b36dc56535820623ebfb1e0424ffbe58
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-19 01:39:03 -0300

    feat: classe mapperAccount criado

---
+++ src/main/java/com/dev/hakeem/nextcash/web/response/AccountResponse.java
@@ -0,0 +1,46 @@
+package com.dev.hakeem.nextcash.web.response;
+
+import com.dev.hakeem.nextcash.enums.AccountType;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AccountResponse {
+    private Long id;
+
+    @NotBlank(message = "Name cannot be blank")
+    private String name;
+
+    @NotNull(message = "Balance cannot be null")
+    @Positive(message = "Balance must be positive")
+    private Double balance;
+
+    @NotBlank(message = "Financial institution cannot be blank")
+    private String financialInstitution;
+
+    @NotNull(message = "Account type cannot be null")
+    private AccountType accountType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AccountResponse that = (AccountResponse) o;
+        return Objects.equals(id, that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
