47bcd0713662df99e49281c84588360567c99c80
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-19 21:27:22 -0300

    feat:  commit

--- src/main/java/com/dev/hakeem/nextcash/web/mapper/ToAccountMapper.java
+++ src/main/java/com/dev/hakeem/nextcash/web/mapper/ToAccountMapper.java
@@ -1,24 +1,51 @@
 package com.dev.hakeem.nextcash.web.mapper;
 
 import com.dev.hakeem.nextcash.entity.Account;
+import com.dev.hakeem.nextcash.entity.User;
+import com.dev.hakeem.nextcash.repository.UserRepository;
+import com.dev.hakeem.nextcash.web.exception.BusinessException;
 import com.dev.hakeem.nextcash.web.request.AccountRequest;
 import com.dev.hakeem.nextcash.web.response.AccountResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@Component
 public class ToAccountMapper {
 
+    private  final UserRepository userRepository;
+     @Autowired
+    public ToAccountMapper(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-    public Account ToRequast(AccountRequest request){
+
+    public Account toRequast(AccountRequest request) {
+
         Account account = new Account();
+
 
         account.setName(request.getName());
         account.setFinancialInstitution(request.getFinancialInstitution());
         account.setAccountType(request.getAccountType());
         account.setBalance(request.getBalance());
+
+        // Associa o usuário à conta
+        // Aqui, você precisa usar o ID do usuário para buscar o usuário correspondente e definir na conta
+        Optional<User> userOptional = userRepository.findById(request.getUser());
+        if (userOptional.isPresent()) {
+            account.setUser(userOptional.get());
+        } else {
+            throw new BusinessException("Usuário não encontrado");
+        }
+
         return account;
     }
 
 
-    public AccountResponse ToReponse(Account account){
+
+    public AccountResponse toReponse(Account account){
         AccountResponse response =  new AccountResponse();
         response.setId(account.getId());
         response.setName(account.getName());
