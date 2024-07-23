104b504c2606b0d9f32dabd4dc663be8a8b8d625
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-23 16:59:42 -0300

    feat: commit

--- src/main/java/com/dev/hakeem/nextcash/repository/TranssactionRepository.java
+++ src/main/java/com/dev/hakeem/nextcash/repository/TranssactionRepository.java
@@ -3,5 +3,5 @@ package com.dev.hakeem.nextcash.repository;
 import com.dev.hakeem.nextcash.entity.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface transsactionRepository extends JpaRepository<Transaction , Long> {
+public interface TranssactionRepository extends JpaRepository<Transaction , Long> {
 }
