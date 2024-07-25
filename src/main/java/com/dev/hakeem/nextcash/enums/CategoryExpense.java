36400d40c7af39acbd7d1ba956a5c5d5fd8d9db1
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-25 10:23:06 -0300

    feat: CategoryExpense e CategoryIncome criados

---
+++ src/main/java/com/dev/hakeem/nextcash/enums/CategoryExpense.java
@@ -0,0 +1,14 @@
+package com.dev.hakeem.nextcash.enums;
+
+public enum CategoryExpense {
+
+    ALIMENTACAO,
+    TRANSPORTE,
+    MORADIA,
+    SAUDE,
+    EDUCACAO,
+    LAZER,
+    SERVICOS,
+    ROUPAS,
+    IMPOSTOS;
+}
---
+++ src/main/java/com/dev/hakeem/nextcash/enums/CategoryIncome.java
@@ -0,0 +1,12 @@
+package com.dev.hakeem.nextcash.enums;
+
+public enum CategoryIncome {
+
+    SALARIO,
+    BONUS,
+    DIVIDENDOS,
+    VENDAS,
+    HORAEXTRAS,
+    DECIMOTERCEIRO,
+    INVESTIMENTO;
+}
