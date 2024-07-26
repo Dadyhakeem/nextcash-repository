44c5600f35e528e3c7a1a355847ccab8515ab5e0
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-26 12:56:34 -0300

    feat:classe IncomeRepository criado

---
+++ src/main/java/com/dev/hakeem/nextcash/repository/IncomeRepository.java
@@ -0,0 +1,7 @@
+package com.dev.hakeem.nextcash.repository;
+
+import com.dev.hakeem.nextcash.entity.Income;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IncomeRepository extends JpaRepository<Income, Long> {
+}
