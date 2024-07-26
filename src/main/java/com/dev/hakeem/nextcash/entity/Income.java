168afefa7c15123fc9bb8d5d3382f46c7024f659
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-25 23:12:32 -0300

    feat:classe receita criado

---
+++ src/main/java/com/dev/hakeem/nextcash/entity/Income.java
@@ -0,0 +1,61 @@
+package com.dev.hakeem.nextcash.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tb_expense")
+public class Income {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "transactionId")
+    private Transaction transaction;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private Category category;
+
+    @Column(name = "amount", nullable = false, precision = 2)
+    private Double amount;
+
+    @Column(name = "descricao")
+    private String descricao;
+
+    @ManyToOne
+    @JoinColumn(name = "accountId", nullable = false)
+    private Account account;
+
+    @Column(name = "created_at", updatable = false)
+    private Timestamp createdAt;
+
+    @Column(name = "updated_at")
+    private Timestamp updatedAt;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Income income = (Income) o;
+        return Objects.equals(id, income.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
