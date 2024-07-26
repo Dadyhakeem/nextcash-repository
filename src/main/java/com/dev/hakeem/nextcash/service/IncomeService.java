23a5ee4ccb59a6496b487ac1eda88c4967fd9781
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-26 12:54:48 -0300

    feat:classe IncomeService criado

---
+++ src/main/java/com/dev/hakeem/nextcash/service/IncomeService.java
@@ -0,0 +1,97 @@
+package com.dev.hakeem.nextcash.service;
+
+import com.dev.hakeem.nextcash.entity.Account;
+import com.dev.hakeem.nextcash.entity.Category;
+import com.dev.hakeem.nextcash.entity.Income;
+import com.dev.hakeem.nextcash.entity.Transaction;
+import com.dev.hakeem.nextcash.exception.EntityNotFoundException;
+import com.dev.hakeem.nextcash.repository.AccountRepository;
+import com.dev.hakeem.nextcash.repository.CategoryRepository;
+import com.dev.hakeem.nextcash.repository.IncomeRepository;
+import com.dev.hakeem.nextcash.repository.TranssactionRepository;
+import com.dev.hakeem.nextcash.web.request.IncomeRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class IncomeService {
+
+    private  final IncomeRepository repository;
+
+    private  final AccountRepository accountRepository;
+    private final TranssactionRepository transsactionRepository;
+
+
+    public IncomeService(IncomeRepository repository, AccountRepository accountRepository, TranssactionRepository transsactionRepository) {
+        this.repository = repository;
+        this.accountRepository = accountRepository;
+        this.transsactionRepository = transsactionRepository;
+    }
+
+
+    public Income createIncome(IncomeRequest request){
+
+        // Cria um novo objeto Income
+        Income income = new Income();
+        income.setId(request.getId());
+        income.setDescricao(request.getDescricao());
+        income.setAmount(request.getAmount());
+        income.setCategoryIncome(request.getCategoryIncome());
+
+        // Busca a conta e a transação associadas aos IDs fornecidos
+        Account acc = accountRepository.findById(request.getAccount())
+                .orElseThrow(()-> new EntityNotFoundException("Account   não encontrado"));
+        Transaction transaction = transsactionRepository.findById(request.getTransaction())
+                .orElseThrow(()-> new EntityNotFoundException("Transaction  não encontrado"));
+
+        // Configura a receita com a conta e a transação
+        income.setAccount(acc);
+        income.setTransaction(transaction);
+
+        // Credita o valor na conta
+        creditar(acc, request.getAmount());
+        return repository.save(income);
+    }
+
+    public List<Income> listarTodos(){
+       return repository.findAll();
+    }
+
+    public void deletarPorId(Long id ){
+        Income income = repository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException(String.format("Receita do id %s  não encontrado",id)));
+        repository.delete(income);
+    }
+
+    public Income buscarPorId(Long id){
+        return repository.findById(id)
+                .orElseThrow(()-> new EntityNotFoundException(String.format("Receita do id %s não encontrado",id)));
+    }
+
+    public  Income editarIncome(IncomeRequest request){
+        Income income = repository.findById(request.getId())
+                .orElseThrow(()-> new EntityNotFoundException("Receita não encontrado"));
+        income.setId(request.getId());
+        income.setDescricao(request.getDescricao());
+        income.setAmount(request.getAmount());
+        income.setCategoryIncome(request.getCategoryIncome());
+        Account acc = accountRepository.findById(request.getAccount())
+                .orElseThrow(()-> new EntityNotFoundException("Account   não encontrado"));
+        Transaction transaction = transsactionRepository.findById(request.getTransaction())
+                .orElseThrow(()-> new EntityNotFoundException("Transaction  não encontrado"));
+        income.setAccount(acc);
+        income.setTransaction(transaction);
+        return repository.save(income);
+    }
+
+    private void creditar(Account destino, Double valor) {
+        if (valor <= 0) {
+            throw new IllegalArgumentException("O valor para crédito deve ser positivo.");
+        }
+        destino.setBalance(destino.getBalance() + valor);
+
+    }
+
+
+}
