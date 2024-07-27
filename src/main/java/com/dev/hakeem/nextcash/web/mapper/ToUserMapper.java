04c1749cecf5a2998a61d5bc997097e2844dcf24
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-27 19:48:32 -0300

    feat:classe enum ROLE acrecentada

--- src/main/java/com/dev/hakeem/nextcash/web/mapper/ToUserMapper.java
+++ src/main/java/com/dev/hakeem/nextcash/web/mapper/ToUserMapper.java
@@ -3,7 +3,9 @@ package com.dev.hakeem.nextcash.web.mapper;
 import com.dev.hakeem.nextcash.entity.User;
 import com.dev.hakeem.nextcash.web.request.UserCreateDTO;
 import com.dev.hakeem.nextcash.web.response.UserCreateResponse;
+import org.springframework.stereotype.Component;
 
+@Component
 public class ToUserMapper {
 
     public static User CreateUserTO(UserCreateDTO createDTO){
@@ -16,12 +18,15 @@ public class ToUserMapper {
         return user;
     }
 
-    public static UserCreateResponse ResponseUserTO(User user){
+    public  UserCreateResponse ResponseUserTO(User user){
         UserCreateResponse createResponse = new UserCreateResponse();
 
         createResponse.setId(user.getId());
         createResponse.setEmail(user.getEmail());
         createResponse.setUsername(user.getUsername());
+
+        String role = user.getRole().name().substring("ROLE_".length());
+        createResponse.setRole(role);
 
         return createResponse;
     }
