1aaba5a2b4aab17d879cda3598832a49d81e5ca4
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-27 19:44:29 -0300

    feat:classe CartaoController documentada com Swagger

---
+++ src/main/java/com/dev/hakeem/nextcash/web/controller/CartaoController.java
@@ -0,0 +1,112 @@
+package com.dev.hakeem.nextcash.web.controller;
+
+import com.dev.hakeem.nextcash.entity.Cartao;
+import com.dev.hakeem.nextcash.service.CartaoService;
+import com.dev.hakeem.nextcash.web.exception.ErroMessage;
+import com.dev.hakeem.nextcash.web.mapper.CartaoMapper;
+import com.dev.hakeem.nextcash.web.request.CartaoRequest;
+import com.dev.hakeem.nextcash.web.response.CartaoResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.stream.Collectors;
+@Tag(name = "Cartao",description = "Contem todos as operacoes relativas aos recursos para criar , edition e leitura de uma cartao.")
+@RestController
+@RequestMapping("/api/v1/cartoes")
+public class CartaoController {
+
+    private  final CartaoService service;
+    private  final CartaoMapper mapper;
+
+    @Autowired
+    public CartaoController(CartaoService service, CartaoMapper mapper) {
+        this.service = service;
+        this.mapper = mapper;
+    }
+
+    @Operation(summary = "Criar Cartao",description = "criar Cartao",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Cartao criada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = CartaoResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Recursos nao processado por dados de entrada  invalidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErroMessage.class)))
+            })
+    @PostMapping
+    public ResponseEntity<CartaoResponse> createCartao(@Valid @RequestBody CartaoRequest request){
+        Cartao cartao = service.createCartao(request);
+        CartaoResponse response = mapper.toResponse(cartao);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+
+    @Operation(summary = "Listar todos os cartoes",description = "Recurso pra listar todos os cartoes",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "Todas os cartoes criados",
+                            content = @Content(mediaType = "application/json",
+                                    array  = @ArraySchema(schema = @Schema(implementation = CartaoResponse.class)))),
+            })
+    @GetMapping
+    public ResponseEntity<List<CartaoResponse>> listarTodos(){
+        List<Cartao> cartaos = service.listarTodos();
+        List<CartaoResponse> responses = cartaos.stream().map(mapper::toResponse).collect(Collectors.toList());
+        return ResponseEntity.ok(responses);
+    }
+
+
+    @Operation(summary = "Recuperar cartao pelo id",description = "Recuperar cartao pelo id",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "Cartao recuperado com sucesso",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = CartaoResponse.class))),
+                    @ApiResponse(responseCode = "404",description = "Cartao nao encontrada",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErroMessage.class)))
+            })
+    @GetMapping("/{id}")
+    private ResponseEntity<CartaoResponse> buscarPorId(@Valid @PathVariable Long id){
+        Cartao cartao = service.buscarPorId(id);
+        CartaoResponse response = mapper.toResponse(cartao);
+        return ResponseEntity.ok(response);
+    }
+
+
+    @Operation(summary = "Deletar cartao pelo id",description = "deletar cartao pelo id",
+            responses = {
+                    @ApiResponse(responseCode = "204",description = "Cartao deletada com sucesso",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(responseCode = "404",description = "Cartao nao encontrada",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErroMessage.class)))
+            })
+    @DeleteMapping("/{id}")
+    public  ResponseEntity<Void> deletarPorId(@Valid @PathVariable Long id){
+        service.deletarPorId(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @Operation(summary = "Atualizar cartao", description = " atualizar cartao",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "Cartao atualizada com sucesso",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = CartaoResponse.class))),
+                    @ApiResponse(responseCode = "400",description = "Usuario e cartao  nao confere",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErroMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Cartao nao encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErroMessage.class)))
+            })
+    @PutMapping("/{id}")
+    public ResponseEntity<CartaoResponse> atualizar(@Valid @RequestBody CartaoRequest request,@PathVariable Long id){
+        request.setId(id);
+        Cartao cartao = service.atualizarCartao(request);
+        CartaoResponse response = mapper.toResponse(cartao);
+        return  ResponseEntity.ok(response);
+    }
+
+
+}
