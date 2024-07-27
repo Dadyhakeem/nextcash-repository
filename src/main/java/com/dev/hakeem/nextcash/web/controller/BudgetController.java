031c6d4fcd1da9f123f8f29e6ce36540c676f133
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-27 19:45:30 -0300

    feat:classe budgetController documentada com Swagger

--- src/main/java/com/dev/hakeem/nextcash/web/controller/BudgetController.java
+++ src/main/java/com/dev/hakeem/nextcash/web/controller/BudgetController.java
@@ -2,9 +2,16 @@ package com.dev.hakeem.nextcash.web.controller;
 
 import com.dev.hakeem.nextcash.entity.Budget;
 import com.dev.hakeem.nextcash.service.BudgetService;
+import com.dev.hakeem.nextcash.web.exception.ErroMessage;
 import com.dev.hakeem.nextcash.web.mapper.BudgetMapper;
 import com.dev.hakeem.nextcash.web.request.BudgetRequest;
 import com.dev.hakeem.nextcash.web.response.BudgetResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -12,9 +19,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
-
+@Tag(name = "Budget",description = "Contem todos as operacoes relativas aos recursos para criar , edition e leitura de um budget.")
 @RestController
 @RequestMapping("/api/v1/budgets")
 public class BudgetController {
@@ -26,18 +32,46 @@ public class BudgetController {
         this.service = service;
         this.mapper = mapper;
     }
+
+
+    @Operation(summary = "Criar Budget",description = "criar Budget",
+            responses = {
+                    @ApiResponse(responseCode = "201", description = "Budget criada com sucesso",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = BudgetResponse.class))),
+                    @ApiResponse(responseCode = "400", description = "Recursos nao processado por dados de entrada  invalidos",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErroMessage.class)))
+            })
     @PostMapping
     public ResponseEntity<BudgetResponse> createBudget(@RequestBody BudgetRequest request){
         Budget budget = service.createBudget(request);
         BudgetResponse response = mapper.toResponse(budget);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
+
+
+    @Operation(summary = "Listar todos as budget",description = "Recurso pra listar todos as budget",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "Todas as budget criados",
+                            content = @Content(mediaType = "application/json",
+                                    array  = @ArraySchema(schema = @Schema(implementation = BudgetResponse.class)))),
+            })
     @GetMapping
     public ResponseEntity<List<BudgetResponse>> listarTodas(){
         List<Budget> budgets = service.listarTodas();
         List<BudgetResponse> responses = budgets.stream().map(mapper::toResponse).collect(Collectors.toList());
         return ResponseEntity.ok(responses);
     }
+
+
+    @Operation(summary = "Atualizar budget", description = " atualizar budget",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "Budget atualizada com sucesso",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = BudgetResponse.class))),
+                    @ApiResponse(responseCode = "400",description = "Usuario e budget  nao confere",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErroMessage.class))),
+                    @ApiResponse(responseCode = "404", description = "Budget nao encontrado",
+                            content = @Content(mediaType = "application/json", schema = @Schema(implementation = ErroMessage.class)))
+            })
     @PutMapping("/{id}")
     public ResponseEntity<BudgetResponse>atualizar(@Valid @PathVariable Long id, @RequestBody BudgetRequest request){
         request.setId(id);
@@ -45,11 +79,27 @@ public class BudgetController {
         BudgetResponse response = mapper.toResponse(budget);
         return ResponseEntity.ok().body(response);
     }
+
+    @Operation(summary = "Deletar budget pelo id",description = "deletar budget pelo id",
+            responses = {
+                    @ApiResponse(responseCode = "204",description = "budget deletada com sucesso",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = Void.class))),
+                    @ApiResponse(responseCode = "404",description = "budget nao encontrada",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErroMessage.class)))
+            })
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deletarPeloId(@Valid @PathVariable Long id){
         service.deletaPeloId(id);
         return ResponseEntity.noContent().build();
     }
+
+    @Operation(summary = "Recuperar budget pelo id",description = "Recuperar budget pelo id",
+            responses = {
+                    @ApiResponse(responseCode = "200",description = "Budget recuperado com sucesso",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = BudgetResponse.class))),
+                    @ApiResponse(responseCode = "404",description = "Budget nao encontrada",
+                            content = @Content(mediaType = "application/json",schema = @Schema(implementation = ErroMessage.class)))
+            })
     @GetMapping("/{id}")
     public ResponseEntity<BudgetResponse> buscarPeloId(@Valid @PathVariable Long id){
         Budget budget = service.buscarPeloId(id);
