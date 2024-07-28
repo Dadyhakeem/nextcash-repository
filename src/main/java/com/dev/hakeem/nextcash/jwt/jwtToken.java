9f1eb50bdf6ff565f06be448de10330e2dad4685
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-28 16:32:52 -0300

    feat: classe jwtToken criado

---
+++ src/main/java/com/dev/hakeem/nextcash/jwt/jwtToken.java
@@ -0,0 +1,14 @@
+package com.dev.hakeem.nextcash.jwt;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter @Setter
+@AllArgsConstructor @NoArgsConstructor
+public class jwtToken {
+
+    private String  token;
+}
