cfd9383ca53b39829e162dd1ab40f3ea12452fd4
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-28 17:01:11 -0300

    feat: classe jwtUserDetails criado

---
+++ src/main/java/com/dev/hakeem/nextcash/jwt/JwtUserDetails.java
@@ -0,0 +1,26 @@
+package com.dev.hakeem.nextcash.jwt;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Collection;
+
+public class JwtUserDetails extends User {
+
+    private com.dev.hakeem.nextcash.entity.User user;
+    public JwtUserDetails(com.dev.hakeem.nextcash.entity.User user) {
+        super(user.getEmail() ,user.getPassword(), AuthorityUtils.createAuthorityList(user.getRole().name()));
+        this.user = user;
+    }
+
+    public  Long getId(){
+        return this.user.getId();
+    }
+
+    public  String getRole(){
+        return user.getRole().name();
+    }
+
+
+}
