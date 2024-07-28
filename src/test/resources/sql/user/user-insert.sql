4211bf649ae328ce008153dd635057b681c83db7
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-27 22:40:07 -0300

    feat:sql para test

---
+++ src/test/resources/sql/user/user-insert.sql
@@ -0,0 +1,3 @@
+insert into tb_users(id,email,username,password,role) values(100, 'dady@gmail.com', 'Dady', '123456', 'ROLE_ADMIN');
+insert into tb_users(id,email,username,password,role) values(101, 'ciro@gmail.com', 'Ciro', '123456', 'ROLE_CLIENT');
+insert into tb_users(id,email,username,password,role) values(102, 'taizy@gmail.com', 'Taizy', '123456', 'ROLE_CLIENT');
