36f4a66feb6c5337529eb5e0701083964b96bb94
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-29 17:54:33 -0300

    feat:  classe Authentication

--- src/main/java/com/dev/hakeem/nextcash/web/exception/ApiExceptionHandler.java
+++ src/main/java/com/dev/hakeem/nextcash/web/exception/ApiExceptionHandler.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -64,6 +65,18 @@ public class ApiExceptionHandler {
                 .status(HttpStatus.BAD_REQUEST)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(new ErroMessage(request,HttpStatus.BAD_REQUEST, ex.getMessage()));
+
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<ErroMessage> AccessDeniedException(AccessDeniedException ex,
+                                                                HttpServletRequest request
+    ){
+        log.error("api Error - ",ex);
+        return ResponseEntity
+                .status(HttpStatus.FORBIDDEN)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(new ErroMessage(request,HttpStatus.FORBIDDEN, ex.getMessage()));
 
     }
 }
