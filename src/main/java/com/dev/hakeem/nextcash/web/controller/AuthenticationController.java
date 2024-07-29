e49f55d1707313d7fa09fafb5ab27e758bc54067
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-29 17:54:18 -0300

    feat:  classe Authentication   criado

--- src/main/java/com/dev/hakeem/nextcash/web/controller/AuthenticationController.java
+++ src/main/java/com/dev/hakeem/nextcash/web/controller/AuthenticationController.java
@@ -9,6 +9,7 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -25,7 +26,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthenticationController {
 
     private static final Logger log = LoggerFactory.getLogger(AuthenticationController.class);
+    @Autowired
     private final JwtUserDetailsService service;
+    @Autowired
     private final AuthenticationManager manager;
 
 
