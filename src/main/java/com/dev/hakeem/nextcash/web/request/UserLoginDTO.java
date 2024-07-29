86a3ae3fef3e2a03f3e7566f0d04d68c939e9552
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-28 21:00:08 -0300

    feat: classe UserLoginDTO criado

---
+++ src/main/java/com/dev/hakeem/nextcash/web/request/UserLoginDTO.java
@@ -0,0 +1,24 @@
+package com.dev.hakeem.nextcash.web.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserLoginDTO {
+
+    @NotBlank
+    @Email(message = "Formato do email esta invalido",regexp = "^[a-z0-9.+-]+@[a-z0-9.-]+\\.[a-z]{2,}$")
+    private  String email;
+
+    @NotBlank
+    @Size(min = 6, max = 6, message = "Password must be exactly 6 characters long")
+    private String password;
+}
