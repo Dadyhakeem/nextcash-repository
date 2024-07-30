74219e72d31061665e77dc422f410ab324a9e5c1
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-30 13:16:38 -0300

    feat: classe cliente criado

---
+++ src/main/java/com/dev/hakeem/nextcash/entity/Client.java
@@ -0,0 +1,59 @@
+package com.dev.hakeem.nextcash.entity;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.validator.constraints.br.CPF;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Setter @Getter
+@AllArgsConstructor @NoArgsConstructor
+@Entity
+@Table(name = "clients")
+@EntityListeners(AuditingEntityListener.class)
+public class Client implements Serializable {
+private  static  final long serialverion = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "name",nullable = false,length = 100)
+    private String name;
+    @CPF
+    @Column(name = "cpf",nullable = false,unique = true,length = 11)
+    private String cpf;
+    @OneToOne
+    @JoinColumn(name = "userid",nullable = false)
+    private User user;
+    @CreatedBy
+    @Column(name = "createdAt")
+    private LocalDateTime createdAt;
+    @LastModifiedBy
+    @Column(name = "updateddAt")
+    private LocalDateTime updateddAt;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Client client = (Client) o;
+        return Objects.equals(id, client.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
+
