ef61065d41d64f1665d8a7a831a8f67b00fa7f7b
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-30 14:50:03 -0300

    feat: exeption CpfUniqueViolationExeption criado

---
+++ src/main/java/com/dev/hakeem/nextcash/exception/CpfUniqueViolationExeption.java
@@ -0,0 +1,7 @@
+package com.dev.hakeem.nextcash.exception;
+
+public class CpfUniqueViolationExeption extends RuntimeException {
+    public CpfUniqueViolationExeption(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/dev/hakeem/nextcash/web/mapper/ClientMapper.java
+++ src/main/java/com/dev/hakeem/nextcash/web/mapper/ClientMapper.java
@@ -1,43 +1,23 @@
 package com.dev.hakeem.nextcash.web.mapper;
 
+
 import com.dev.hakeem.nextcash.entity.Client;
-import com.dev.hakeem.nextcash.entity.User;
-import com.dev.hakeem.nextcash.exception.EntityNotFoundException;
 import com.dev.hakeem.nextcash.repository.ClientRepository;
-import com.dev.hakeem.nextcash.repository.UserRepository;
 import com.dev.hakeem.nextcash.web.request.ClientResquest;
 import com.dev.hakeem.nextcash.web.response.ClientResp;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.client.ClientResponse;
-import org.springframework.web.reactive.function.client.support.ClientResponseWrapper;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import org.modelmapper.ModelMapper;
 
-@Component
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ClientMapper {
 
-    private  final ClientRepository repository;
-    private final UserRepository userRepository;
-     @Autowired
-    public ClientMapper(ClientRepository repository, UserRepository userRepository) {
-        this.repository = repository;
-         this.userRepository = userRepository;
-     }
+  public static Client toClient(ClientResquest resquest){
+      return  new ModelMapper().map(resquest,Client.class);
+  }
 
-    public Client toResquest(ClientResquest resquest){
-         Client client = new Client();
-         client.setName(resquest.getName());
-         client.setCpf(resquest.getCpf());
-        User user = userRepository.findById(resquest.getUser())
-                .orElseThrow(()-> new EntityNotFoundException("User nao encontrada"));
-        client.setUser(user);
-        return client;
+    public static ClientResp toReponse(Client client){
+        return  new ModelMapper().map(client,ClientResp.class);
     }
 
-    public ClientResp toResponse(Client client){
-         ClientResp response = new ClientResp();
-         response.setId(client.getId());
-         response.setName(client.getName());
-         response.setCpf(client.getCpf());
-         return response;
-    }
 }
