ab8466a6f26264c18dbe764a325ea0b6928af1f2
Dadyhakeem <dadyfranciscomendes@gmail.com>
2024-07-30 02:19:42 -0300

    feat: teste  de autenticacao com status 400

--- src/test/java/com/dev/hakeem/nextcash/AuthenticacaoIT.java
+++ src/test/java/com/dev/hakeem/nextcash/AuthenticacaoIT.java
@@ -1,8 +1,10 @@
 package com.dev.hakeem.nextcash;
 
 import com.dev.hakeem.nextcash.jwt.jwtToken;
+import com.dev.hakeem.nextcash.web.exception.ErroMessage;
 import com.dev.hakeem.nextcash.web.request.UserLoginDTO;
 import org.junit.jupiter.api.Test;
+import org.springdoc.api.ErrorMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
@@ -32,6 +34,36 @@ public class AuthenticacaoIT {
 
         org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
     }
+
+    @Test
+    public void autenticar_ComCredenciaisInvalidas_RetornarErrorMessageStatus400() {
+        ErroMessage responseBody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserLoginDTO("invalido@email.com", "123456"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ErroMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responseBody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
+
+        ErroMessage responsbody = testClient
+                .post()
+                .uri("/api/v1/auth")
+                .contentType(MediaType.APPLICATION_JSON)
+                .bodyValue(new UserLoginDTO("amor@email.com", "000000"))
+                .exchange()
+                .expectStatus().isBadRequest()
+                .expectBody(ErroMessage.class)
+                .returnResult().getResponseBody();
+
+        org.assertj.core.api.Assertions.assertThat(responsbody).isNotNull();
+        org.assertj.core.api.Assertions.assertThat(responseBody.getStatus()).isEqualTo(400);
+    }
+
 
 
 
